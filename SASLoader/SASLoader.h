ee73a9e78475a1bf809e2991bf1c097693a95483
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-04-06 21:35:40 +0530

    Initial Commit

---
+++ SASLoader/SASLoader.h
@@ -0,0 +1,19 @@
+//
+//  SASLoader.h
+//  SASLoader
+//
+//  Created by Manu Puthoor on 06/04/20.
+//  Copyright © 2020 Manu Puthoor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SASLoader.
+FOUNDATION_EXPORT double SASLoaderVersionNumber;
+
+//! Project version string for SASLoader.
+FOUNDATION_EXPORT const unsigned char SASLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SASLoader/PublicHeader.h>
+
+
